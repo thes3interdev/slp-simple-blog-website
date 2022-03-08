78e519ed75911504b9e416045dddf4511daf8f54
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 02:25:59 +0300

    feat: new feature.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from '../components/BlogList';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState([
@@ -30,12 +31,7 @@ const Home = () => {
 
 	return (
 		<div className="home">
-			{blogs.map((blog) => (
-				<div className="blog-preview" key={blog.id}>
-					<h2 className="blog-preview-header">{blog.title}</h2>
-					<p>Written by: {blog.author}</p>
-				</div>
-			))}
+			<BlogList blogs={blogs} title="All Blogs" />
 		</div>
 	);
 };
