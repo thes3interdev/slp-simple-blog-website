e6572a64a64638bbcc204c4fa2afd64cd08bb8ae
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 00:24:30 +0300

    styles: adding styles.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,9 @@ const Navbar = () => {
 			<h1 className="navbar-header">The Simple Blog</h1>
 			<div className="navbar-links">
 				<a href="/">Home</a>
-				<a href="/create">New Blog</a>
+				<a href="/create" className="navbar-button">
+					New Blog
+				</a>
 			</div>
 		</nav>
 	);
