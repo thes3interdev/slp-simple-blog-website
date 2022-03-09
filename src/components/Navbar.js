283239f4a58cc129dc824cff73f2f21a6aeb2e34
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 11:40:00 +0300

    feat: router links.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<h1 className="navbar-header">The Simple Blog</h1>
 			<div className="navbar-links">
-				<a href="/">Home</a>
-				<a href="/create">Blogs</a>
+				<Link to="/">Home</Link>
+				<Link to="/create">Blogs</Link>
 			</div>
 		</nav>
 	);
