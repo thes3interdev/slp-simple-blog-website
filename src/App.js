59e3c4095758e75c71ac80417120fe1eab6f9a13
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 11:21:07 +0300

    feat: react router.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-
 import Home from './pages/Home';
 
 const App = () => {
 	return (
-		<div>
-			<Navbar />
-			<div className="content">
-				<Home />
+		<Router>
+			<div>
+				<Navbar />
+				<div className="content">
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
+				</div>
 			</div>
-			`
-		</div>
+		</Router>
 	);
 };
 
