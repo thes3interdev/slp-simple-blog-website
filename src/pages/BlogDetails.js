d4a98a050a6cc21523d278e1b02afafa4a54575a
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 13:49:47 +0300

    feat: deleting blogs.

--- src/pages/BlogDetails.js
+++ src/pages/BlogDetails.js
@@ -1,9 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 const BlogDetails = () => {
 	const { id } = useParams();
 	const { data: blog, error, isLoading } = useFetch('http://localhost:5000/blogs/' + id);
+	const navigate = useNavigate();
+
+	const handleClick = () => {
+		fetch('http://localhost:5000/blogs/' + blog.id, {
+			method: 'DELETE',
+		}).then(() => {
+			navigate('/');
+		});
+	};
 
 	return (
 		<div className="blog-details">
@@ -14,6 +23,9 @@ const BlogDetails = () => {
 					<h2 className="blog-details-header">{blog.title}</h2>
 					<p>Written by {blog.author}</p>
 					<div className="blog-details-body">{blog.body}</div>
+					<button className="button" onClick={handleClick}>
+						Delete
+					</button>
 				</article>
 			)}
 		</div>
