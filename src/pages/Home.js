f10faa0c369889b4962f8e6626d9eb09c69cab40
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 04:02:05 +0300

    feat: conditional loading message.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,28 @@ import api from '../api/index';
 
 const Home = () => {
 	const [blogs, setBlogs] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		const fetchBlogs = async () => {
-			try {
-				const response = await api.get('/blogs');
-				setBlogs(response.data);
-			} catch (err) {}
-		};
+		setTimeout(() => {
+			const fetchBlogs = async () => {
+				try {
+					const response = await api.get('/blogs');
+					setBlogs(response.data);
+					setIsLoading(false);
+				} catch (err) {}
+			};
 
-		fetchBlogs();
+			fetchBlogs();
+		}, 1500);
 	}, []);
 
-	return <div className="home">{blogs && <BlogList blogs={blogs} title="All Blogs" />}</div>;
+	return (
+		<div className="home">
+			{isLoading && <div>Loading blogs...</div>}
+			{blogs && <BlogList blogs={blogs} title="All Blogs" />}
+		</div>
+	);
 };
 
 export default Home;
