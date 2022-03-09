139d8e6bf5f87b90f7c0594c36f0a86961b2f3f2
Michael Owuor <michael.owuor@gmail.com>
2022-03-09 03:28:16 +0300

    feat: json server.

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
 	// baseURL: process.env.NODE_ENV === 'production' ? 'https://backend.s3interdev.com/api/' : 'http://localhost:1337/api/',
-	baseURL: 'http://localhost:5000/',
+	baseURL: 'http://localhost:5000/blogs',
 });
